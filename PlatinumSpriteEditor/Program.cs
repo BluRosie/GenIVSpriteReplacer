69a3aa6c67e075cf5a64d3c763420ccacfc239d6
Nathan Wichers <bluroseai@hotmail.com>
2020-11-02 14:03:24 -0500

    pngtoncgr

--- PlatinumSpriteEditor/Program.cs
+++ PlatinumSpriteEditor/Program.cs
@@ -6,17 +6,15 @@ namespace PlatinumSpriteEditor
 	/// <summary>
 	/// Class with program entry point.
 	/// </summary>
-	internal sealed class Program
+	/*internal sealed */class Program
 	{
 		/// <summary>
 		/// Program entry point.
 		/// </summary>
 		[STAThread]
-		private static void Main(string[] args)
+		static void Main(string[] args)
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new MainForm());
+			new MainForm(args);
 		}
 		
 	}
